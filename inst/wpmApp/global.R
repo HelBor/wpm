671fc77ff297f4c24ed0efa08760a6cdee7ab345
Hélène Borges <borges.helene.sophie@gmail.com>
2019-12-17 11:13:49 +0100

    global.r update

--- inst/wpmApp/global.R
+++ inst/wpmApp/global.R
@@ -1,5 +1,6 @@
 # Libraries import
 library(shinydashboard)
+library(shinyWidgets)
 library(DT)
 library(RColorBrewer)
 library(dplyr)
@@ -10,6 +11,7 @@ library(extrafont)
 # import modules and functions from another R files
 source(file.path("modules", "data_import.r"), local = TRUE)
 source(file.path("modules","plate_spec.r"), local = TRUE)
+source(file.path("modules","backtracking.r"), local = TRUE)
 source(file.path("functions", "fonctions.r"), local = TRUE)
 source(file.path("functions", "plotPlateMaps.r"), local = TRUE)
 
--- inst/wpmApp/modules/backtracking.r
+++ inst/wpmApp/modules/backtracking.r
@@ -6,5 +6,13 @@ backtrackUI <- function(id, label = NULL) {
 
 plateSpec <- function(input, output, session, df, df_forbidden, nbh_mode) {
 
-
+  user_data <- reactive({
+    datafile()$Sample.name <- as.integer(datafile()$Sample.name)
+    datafile()$Group <- as.factor(datafile()$Group)
+    datafile()$Well <- as.character(NA)
+    datafile()$Status <- as.factor("allowed")
+    datafile()$Row <- NA
+    datafile()$Column <- NA
+    datafile()
+  })
 }
