9aefbe3c81bf810557116a3b9adc47e096703dd6
H_Borges <borges.helene.sophie@gmail.com>
2020-04-08 18:48:52 +0200

    tests

--- R/wpm.R
+++ R/wpm.R
@@ -6,14 +6,23 @@
 ##' @examples
 ##' wpm()
 ##' @export
-
+print(paste0("(wpm.R-1) working directory: ", getwd()))
 wpm <- function(){
   require(shiny)
-  G <- .GlobalEnv
+  print(paste0("(wpm.R-2) working directory: ", getwd()))
+  # G <- .GlobalEnv
+  print(paste0("(wpm.R-3) working directory: ", getwd()))
   if (interactive()){
+    print(paste0("(wpm.R-4) working directory: ", getwd()))
     options(shiny.maxRequestSize=1024^3)
+    print(paste0("(wpm.R-5) working directory: ", getwd()))
     a=shiny::runApp(system.file("wpmApp",package="wpm"),
                     launch.browser = TRUE)
+    print(paste0("(wpm.R-6) working directory: ", getwd()))
     return(invisible(a))
-  } else {return (NULL)}
+  }
+  print(paste0("(wpm.R-7) working directory: ", getwd()))
+  return (NULL)
+
 }
+print(paste0("(wpm.R) working directory: ", getwd()))
