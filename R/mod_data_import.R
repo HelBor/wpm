cec8d01e9802b51f0b2fe2244f9857305003f884
HelBor <borges.helene.sophie@gmail.com>
2020-11-30 15:59:05 +0100

    visual changes for shiny app upload block

--- R/mod_data_import.R
+++ R/mod_data_import.R
@@ -1,37 +1,44 @@
 mod_data_import_ui <- function(id){
     ns <- shiny::NS(id)
-    
+
     shiny::fluidRow(
         # inputs part
         shiny::column(width = 6,
-            shinydashboard::tabBox(
-                title = "Upload dataset",
-                id = ns("upload_tabset"),
-                side = "right",
+            shinydashboard::box(
+                status = "warning",
                 width = 12,
-                height = "700px",
-                selected = "demo",
-                shiny::tabPanel(
-                    title = "Load the demo dataset",
-                    value = "demo",
-                    icon = shiny::icon("database"),
-                    shiny::column(width=12,
-                        mod_data_import_demo_ui(ns("demo"))
-                    )
+                collapsible = TRUE,
+                solidHeader = TRUE,
+                title = shiny::h3("Upload the dataset"),
 
-                ),
-                shiny::tabPanel(
-                    title = "Upload a file",
-                    value = "user",
-                    icon = shiny::icon("file-upload"),
-                    shiny::column(width=12,
-                        mod_data_import_file_ui(ns("user_file"))
+                shinydashboard::tabBox(
+                    title = "",
+                    id = ns("upload_tabset"),
+                    side = "left",
+                    width = 12,
+                    selected = "user_csv",
+                    shiny::tabPanel(
+                        title = "Upload a file",
+                        value = "user_csv",
+                        icon = shiny::icon("file-upload"),
+                        shiny::column(width = 12,
+                                      mod_data_import_file_ui(ns("csv_file"))
+                        )
+                    ),
+                    shiny::tabPanel(
+                        title = "Load the demo dataset",
+                        value = "demo",
+                        icon = shiny::icon("database"),
+                        shiny::column(width = 12,
+                                      mod_data_import_demo_ui(ns("demo"))
+                        )
+
                     )
                 )
             )
         ),
         # output part
-        shiny::column(width=6,
+        shiny::column(width = 6,
             shiny::fluidRow(
                 shiny::column(
                     width = 8,
@@ -45,7 +52,7 @@ mod_data_import_ui <- function(id){
                     )),
                 shiny::column(
                     width = 4,
-                    style='padding:0px;margin:0px;',
+                    style = 'padding:0px;margin:0px;',
                     shinydashboard::box(style = "overflow-x: scroll;",
                         title = shiny::h3("Preview output template"),
                         solidHeader = TRUE, collapsible = TRUE,
@@ -55,7 +62,6 @@ mod_data_import_ui <- function(id){
                     shinydashboard::valueBoxOutput(ns("nb_ech"), width = 6),
                     shinydashboard::valueBoxOutput(ns("nb_gp"), width = 6)
                 )
-
             )
         )
     )
@@ -67,48 +73,48 @@ mod_data_import_server <- function(id){
     shiny::moduleServer(
         id,
         function(input, output, session) {
-            
+
             ns <- session$ns
-            
+
             toReturn <- shiny::reactiveValues(
                 df = NULL,
                 distinct_gps = NULL,
                 gp_levels = NULL,
                 nb_samples = 0
             )
-            
+
 
             demo_mod <- mod_data_import_demo_server("demo")
-            file_mod <- mod_data_import_file_server("user_file")
+            file_mod <- mod_data_import_file_server("csv_file")
             # complete here if new module of import
-                
+
             df <- shiny::reactive({
                 if(input$upload_tabset == "demo" ){
                     return(demo_mod$df)
 
                 }else{
                     return(file_mod$df)
-                    
+
                 }
                 # complete here if new module of import
             })
-            
-            
-            
+
+
+
             df_wpm <- shiny::reactive({
                 if(input$upload_tabset == "demo" ){
                     return(demo_mod$df_wpm)
-                    
+
                 }else{
                     return(file_mod$df_wpm)
-                    
+
                 }
                 # complete here if new module of import
             })
 
 
-            
-            
+
+
             output$nb_ech <- shinydashboard::renderValueBox({
                 if (is.null(df_wpm())) {
                     shinydashboard::valueBox(
@@ -123,7 +129,7 @@ mod_data_import_server <- function(id){
                         color = "teal")
                 }
             })
-            
+
             ## Vector containing the different group names
             gp_levels <- shiny::reactive({
                 nb <- NULL
@@ -134,7 +140,7 @@ mod_data_import_server <- function(id){
                 }
                 return(nb)
             })
-            
+
             ## The number of distinct groups in the file
             distinct_gps <- shiny::reactive({
                 d_gp <- NULL
@@ -148,7 +154,7 @@ mod_data_import_server <- function(id){
                                 message = "The number of separate groups must not
                         exceed 12.")
                 )
-                
+
                 return(d_gp)
             })
             # the number of samples in the dataset
@@ -160,7 +166,7 @@ mod_data_import_server <- function(id){
                 }
                 return(nb)
             })
-            
+
             output$nb_gp <- shinydashboard::renderValueBox({
                 shinydashboard::valueBox(
                     value = distinct_gps(),
@@ -169,11 +175,11 @@ mod_data_import_server <- function(id){
                     color = "teal")
             })
             shiny::outputOptions(output, "nb_gp", suspendWhenHidden = FALSE)
-            
-            
-            
-            
-            
+
+
+
+
+
             output$default_table <- DT::renderDataTable(
                 if(!is.null(df())){
                     if(methods::is(df(), "data.frame")){
@@ -185,8 +191,8 @@ mod_data_import_server <- function(id){
                     }
                 }
             )
-            
-            
+
+
             output$wpm_table <- DT::renderDataTable(
                 if(!is.null(df_wpm())){
                     if(methods::is(df_wpm(), "data.frame")){
@@ -198,23 +204,23 @@ mod_data_import_server <- function(id){
                     }
                 }
             )
-            
-            
+
+
             output$default_text <- shiny::renderText({
                 if(methods::is(df(), "character")){
                     df()
                 }
             })
-            
 
-                
+
+
             shiny::observe({
-                # 
+                #
                 # print("data import : on est dans le observe du toReturn")
                 # logging::loginfo("nb_s: %s", nb_s())
                 # logging::loginfo("distinct_gps: %s", distinct_gps())
                 # logging::loginfo("gp_levels: %s", gp_levels())
-                # 
+                #
                 # print("-------------------------------------------")
                 toReturn$df <- df_wpm()
                 toReturn$distinct_gps <- distinct_gps()
@@ -222,7 +228,7 @@ mod_data_import_server <- function(id){
                 toReturn$nb_samples <- nb_s()
             })
 
-            
+
             return(toReturn)
         }
     )
